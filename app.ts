1750a37108cc1400f209500dbcb73a03bad500c7
Mykola_Hrytsenko <94016020+MykolaHrytsenko@users.noreply.github.com>
2022-06-25 12:28:30 +0300

    save

--- app.ts
+++ app.ts
@@ -1,11 +1,56 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
 
-function add(num1: number, num2: number) {
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value));
+// });
+
+// let age: number;
+// age = 50;
+// let name2: string;
+// name2 = "Max";
+// let toggle: boolean;
+// toggle = true;
+// let empty: null;
+// empty = null;
+// let notInitialize: undefined;
+// notInitialize = undefined;
+// let callback = (a: number) => number;
+// callback = (a) => {
+//   return 100 + a;
+// };
+
+// let anything: any;
+// anything = -20;
+// anything = "Text";
+// anything = {};
+
+// let some: unknown;
+// some = "Text";
+
+// let str: string;
+
+// if (typeof some === "string") {
+//   str = some;
+// }
+
+// let person: [string, number];
+
+// person = ["Max", 21];
+
+function showMessage(message) {
+  console.log(message);
+}
+
+function calc(num1, num2) {
   return num1 + num2;
 }
 
-button.addEventListener("click", function () {
-  console.log(add(+input1.value, +input2.value));
-});
+function customError() {
+  throw new Error("Error");
+}
